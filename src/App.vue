68afdaa62f8cebf816410cff0f8468853ff61eb6
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-13 22:18:22 +0200

    create the initial products container component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductsContainer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import ProductsContainer from "@/components/ProductsContainer";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductsContainer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*global style*/
+body {
+  background-color: #e7e9e9;
 }
 </style>
---
+++ src/components/ProductsContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="products-container">
+  </div>
+</template>
+
+<script>
+export default {
+name: "ProductsContainer"
+}
+</script>
+
+<style scoped>
+.products-container {
+  margin-top: 40px;
+  background-color: #fff;
+  padding: 20px;
+}
+</style>
