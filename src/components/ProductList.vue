730f4300fefc4b1eee368138a7cdeb76ebad7515
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-13 22:25:14 +0200

    feat: create a component to display all products

---
+++ src/components/ProductList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Please Display All Products</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    products: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ProductsContainer.vue
+++ src/components/ProductsContainer.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="products-container">
+    <div class="container-fluid">
+<!--      for applied filter and sort by and number of results-->
+        <div class="row">
+          <div class="row">
+            <div class="col-md-10 col-12">
+                <ProductList :products="[]" />
+            </div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProductList from "@/components/ProductList";
 export default {
-name: "ProductsContainer"
+name: "ProductsContainer",
+  components: {ProductList}
 }
 </script>
 
